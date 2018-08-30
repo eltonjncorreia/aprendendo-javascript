7cee5829f6998bdd4c783aff5e1cd04cbfe5ef43
eltonjncorreia <eltonjncorreia@gmail.com>
2018-08-30 17:25:02 -0300

    operador ternario

---
+++ aula_7_op_ternario.js
@@ -0,0 +1,14 @@
+var sexo = "M"
+
+var res;
+        
+if (sexo == "F"){
+    res = true
+
+} else {
+    res = false
+};
+
+// bastante util quando tem um if else 
+var retorno = (sexo === "M") ? true : false;
+console.log(retorno);
