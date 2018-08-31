887bf2db831dda6dc4452d478a3cc21995d7783e
eltonjncorreia <eltonjncorreia@gmail.com>
2018-08-30 22:38:56 -0300

    organizando estrutura

---
+++ exercicios/exe_01.js
@@ -0,0 +1,12 @@
+var endereco = {
+    nome: 'Rua dos pinheiros',
+    numero: '1293',
+    bairro: 'Centro',
+    cidade: 'São Paulo',
+    uf: 'SP'
+};
+
+var titulo_exe = "Exercicio 01"
+
+alert(titulo_exe + " O usuario mora em "+ endereco.cidade + ", bairro: " + 
+ endereco.bairro+ ", na rua "+ endereco.nome+ " com nº "+ endereco.numero)
---
+++ exercicios/exe_03.js
@@ -0,0 +1,10 @@
+function temHabilidade(skills) {
+    for (i = 0; i <= skills.length; i++){
+        if (skills[i] == 'Javascript'){
+            console.log(true)
+        }
+    }
+};   
+
+var skills = ['Javascript', 'ReactJS', 'React Native'];
+temHabilidade(skills);
