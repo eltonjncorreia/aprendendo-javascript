534514e7e1da40a32095d1cd2000d7ea311a9ad9
eltonjncorreia <eltonjncorreia@gmail.com>
2018-09-05 03:37:18 -0300

    lib axios

--- promisses.js
+++ promisses.js
@@ -1,25 +1,35 @@
-var minhaPromise = function(){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/eltonjncorreia');
-        xhr.send(null);
+/// FAZER REQUISICAO COM JAVASCRIPT PURO
+// var minhaPromise = function(){
+//     return new Promise(function(resolve, reject){
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://api.github.com/users/eltonjncorreia');
+//         xhr.send(null);
 
-        xhr.onreadystatechange = function(){
-            if (xhr.readyState === 4){
-                if (xhr.status  === 200){
-                    resolve(JSON.parse(xhr.responseText));
+//         xhr.onreadystatechange = function(){
+//             if (xhr.readyState === 4){
+//                 if (xhr.status  === 200){
+//                     resolve(JSON.parse(xhr.responseText));
 
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
+//                 } else {
+//                     reject('Erro na requisição');
+//                 }
+//             }
+//         }
+//     });
+// }
 
-minhaPromise()
+// minhaPromise()
+//     .then(function(response){
+//         console.log(response);
+//     })
+//     .catch(function(error){
+//         console.warn(error);
+//     });
+
+// FAZER REQUEST COM LIB 'Axios'
+axios.get('https://api.github.com/users/eltonjncorreia')
     .then(function(response){
-        console.log(response);
+        console.log(response.data.avatar_url);
     })
     .catch(function(error){
         console.warn(error);
